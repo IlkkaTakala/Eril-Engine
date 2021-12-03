c92b6e367b6b72bcc331dbf8ee9341e0dd819538
Seiteri Matti TTV20SP <mattiseiteri@kamk.fi>
2021-12-03 12:41:41 +0200

    sceneloading components updated

--- Engine/Core/Objects/InstancedObject.cpp
+++ Engine/Core/Objects/InstancedObject.cpp
@@ -11,21 +11,46 @@ void InstancedObject::LoadWithParameters(const String& args)
 	auto data = ParseOptions(args);
 
 	auto ins = data.find("Instances");
+	auto ran = data.find("Randomize");
+	auto sca = data.find("InScale");
+	auto rot = data.find("InRotation");
 
+	int randCount = 0;
+	int instanceCount = 0;
+	std::vector<String> it;
+	Vector Scale = Vector(1, 1, 1);
+	Vector Rotation = Vector(0, 0, 0);
+	
 
+	if (ran != data.end()) randCount = atoi(ran->second.c_str());
+	if (sca != data.end()) Scale = Vector(sca->second);
+	if (rot != data.end()) Rotation = Vector(rot->second);;
 	if (ins != data.end()) {
-		auto it = split(ins->second, '-');
-		Transformation* arr = new Transformation[it.size() / 3]();
-		for (int i = 0; i < it.size() / 3; i++)
-		{
-			arr[i].Location = Vector(it[i * 3]);
-			arr[i].Scale = Vector(it[i * 3 + 2]);
-			arr[i].Rotation = Vector(it[i * 3 + 1]);
-		}
-		AddInstances(it.size() / 3, arr);
-		delete[] arr;
+		it = split(ins->second, '-');
+		instanceCount = it.size() / 3;
 	}
+		Transformation* arr = new Transformation[randCount + instanceCount]();
+		for (int i = 0; i < randCount; i++)
+		{
+			float x = rand() % 100 - 50.f;
+			float y = rand() % 100 - 50.f;
+
+			arr[i].Location = Vector(x, y, 0.f);
+			arr[i].Scale = Scale;
+			arr[i].Rotation = Rotation;
+		}
+
+		for (int i = 0; i < instanceCount ; i++)
+		{
+			arr[i + randCount].Location = Vector(it[i * 3]);
+			arr[i + randCount].Scale = Vector(it[i * 3 + 2]);
+			arr[i + randCount].Rotation = Vector(it[i * 3 + 1]);
+		}
+
+		AddInstances(randCount + instanceCount, arr);
+		delete[] arr;
 }
+
 
 void InstancedObject::AddInstances(int count, Transformation* dispArray)
 {
